442cb75ee785db8ac81ab4d827a131e9ae6ceca3
Harun <harunurrashid97.ctg@gmail.com>
2022-10-25 08:04:47 +0600

    its ok

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -25,3 +25,33 @@
 </form>
 
 @endsection
+
+@extends('app')
+
+
+@section('content')
+
+
+
+<form method="post" enctype="multipart/form-data" action="{{ route('category.store' }}">
+    @csrf
+    <div class="row">
+        <div class="col-6">
+            <div class="from-group">
+            <label for="name">Category Name</label>
+            <input class="from-control" name="category" id="name" type="text">
+            </div>
+        </div>
+        <div class="col-6">
+            <div class="from-group">
+            <label for="image">Image</label>
+            <input class="from-control" name="category" id="image" type="file">
+            </div>
+        </div>
+
+    </div>
+
+
+</form>
+
+@endsection
